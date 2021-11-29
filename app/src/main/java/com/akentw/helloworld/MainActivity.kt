34ca0f172462c404527b312de07de37768a0d906
ken.lin.home <kenl5487@gmail.com>
2021-11-29 21:12:42 +0800

    first commit from home

---
+++ app/src/main/java/com/akentw/helloworld/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.akentw.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    // get reference to button
+        val btn_click_me = findViewById(R.id.button) as Button
+        var myTextView = findViewById(R.id.textView) as TextView
+        var timeClicked = 0
+    // set on-click listener
+        btn_click_me.setOnClickListener {
+            timeClicked = timeClicked +  1
+            myTextView.text = timeClicked.toString()
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+    //
+
+    }
+}
